ef40a58a0ac7adca7c7b37a12d49619d1a3a511c
CryptoCase <caseyscottyoung@gmail.com>
2019-08-18 12:23:02 -0500

    adds School

---
+++ src/SchoolTutorial/main.go
@@ -0,0 +1,19 @@
+package main
+
+import (
+	"github.com/CryptoCase/goworkspace/SchoolTutorial/school"
+	//Must import the school package because that contains handler.go and model.go
+	"net/http"
+)
+
+func main() {
+
+	http.HandleFunc("/", school.Index)
+	http.HandleFunc("/index", school.Index)
+
+	// Hands off the functioning to the handler, routes the execution path.
+	http.HandleFunc("/instructor/index", school.InstructorIndex)
+
+	//Type into browser "http://localhost:8080". I increment this by one for each new compile.
+	http.ListenAndServe(":8086", nil)
+}
