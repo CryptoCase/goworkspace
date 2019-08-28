fc2fe33d4a120f8087b12a58694237316664f4be
CryptoCase <caseyscottyoung@gmail.com>
2019-08-27 22:44:35 -0500

    updates portfolio structure

--- src/portfolio/main.go
+++ src/portfolio/main.go
@@ -9,11 +9,4 @@ import (
 func main() {
 	http.Handle("/", http.FileServer(http.Dir("static")))
 	appengine.Main()
-
-	http.HandleFunc("/301", redirect)
-}
-
-func redirect(w http.ResponseWriter, r *http.Request) {
-
-	http.Redirect(w, r, "http://www.golang.org", 301)
 }
